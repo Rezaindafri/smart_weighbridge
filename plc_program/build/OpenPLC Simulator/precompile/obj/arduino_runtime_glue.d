514325e96f4d113691abe359ccb4bb987571f0fc
RifkiReysaad <rifkireysaad20@gmail.com>
2026-07-15 11:06:30 +0700

    Menambahkan file program Ladder Diagram OpenPLC (Rifki)

---
+++ plc_program/build/OpenPLC Simulator/precompile/obj/arduino_runtime_glue.d
@@ -0,0 +1,37 @@
+/home/rifkireysaad/Documents/KP/WeightBride/build/OpenPLC\ Simulator/precompile/obj/arduino_runtime_glue.o: \
+ /home/rifkireysaad/Documents/KP/WeightBride/build/OpenPLC\ Simulator/precompile/sources/arduino_runtime_glue.cpp \
+ /home/rifkireysaad/Documents/KP/WeightBride/build/OpenPLC\ Simulator/src/arduino_runtime_glue.h \
+ /home/rifkireysaad/Documents/KP/WeightBride/build/OpenPLC\ Simulator/src/openplc.h \
+ /home/rifkireysaad/Documents/KP/WeightBride/build/OpenPLC\ Simulator/src/generated.hpp \
+ /home/rifkireysaad/Documents/KP/WeightBride/build/OpenPLC\ Simulator/src/iec_types.hpp \
+ /tmp/openplc-avr-libstdcpp-4.2.7/include/cstdint \
+ /tmp/openplc-avr-libstdcpp-4.2.7/include/cstddef \
+ /home/rifkireysaad/Documents/KP/WeightBride/build/OpenPLC\ Simulator/src/iec_var.hpp \
+ /tmp/openplc-avr-libstdcpp-4.2.7/include/type_traits \
+ /tmp/openplc-avr-libstdcpp-4.2.7/include/utility \
+ /home/rifkireysaad/Documents/KP/WeightBride/build/OpenPLC\ Simulator/src/iec_array.hpp \
+ /tmp/openplc-avr-libstdcpp-4.2.7/include/array \
+ /home/rifkireysaad/Documents/KP/WeightBride/build/OpenPLC\ Simulator/src/iec_fault.hpp \
+ /home/rifkireysaad/Documents/KP/WeightBride/build/OpenPLC\ Simulator/src/iec_located.hpp \
+ /home/rifkireysaad/Documents/KP/WeightBride/build/OpenPLC\ Simulator/src/iec_std_lib.hpp \
+ /home/rifkireysaad/Documents/KP/WeightBride/build/OpenPLC\ Simulator/src/iec_traits.hpp \
+ /home/rifkireysaad/Documents/KP/WeightBride/build/OpenPLC\ Simulator/src/iec_retain.hpp \
+ /home/rifkireysaad/Documents/KP/WeightBride/build/OpenPLC\ Simulator/src/iec_ptr.hpp \
+ /home/rifkireysaad/Documents/KP/WeightBride/build/OpenPLC\ Simulator/src/iec_string.hpp \
+ /tmp/openplc-avr-libstdcpp-4.2.7/include/cstdio \
+ /tmp/openplc-avr-libstdcpp-4.2.7/include/cstring \
+ /tmp/openplc-avr-libstdcpp-4.2.7/include/cstdlib \
+ /tmp/openplc-avr-libstdcpp-4.2.7/include/algorithm \
+ /home/rifkireysaad/Documents/KP/WeightBride/build/OpenPLC\ Simulator/src/iec_wstring.hpp \
+ /home/rifkireysaad/Documents/KP/WeightBride/build/OpenPLC\ Simulator/src/iec_time.hpp \
+ /home/rifkireysaad/Documents/KP/WeightBride/build/OpenPLC\ Simulator/src/iec_date.hpp \
+ /home/rifkireysaad/Documents/KP/WeightBride/build/OpenPLC\ Simulator/src/iec_dt.hpp \
+ /home/rifkireysaad/Documents/KP/WeightBride/build/OpenPLC\ Simulator/src/iec_tod.hpp \
+ /tmp/openplc-avr-libstdcpp-4.2.7/include/cmath \
+ /tmp/openplc-avr-libstdcpp-4.2.7/include/chrono \
+ /home/rifkireysaad/Documents/KP/WeightBride/build/OpenPLC\ Simulator/src/iec_enum.hpp \
+ /home/rifkireysaad/Documents/KP/WeightBride/build/OpenPLC\ Simulator/src/iec_memory.hpp \
+ /home/rifkireysaad/Documents/KP/WeightBride/build/OpenPLC\ Simulator/src/iec_pointer.hpp \
+ /tmp/openplc-avr-libstdcpp-4.2.7/include/string \
+ /home/rifkireysaad/Documents/KP/WeightBride/build/OpenPLC\ Simulator/src/debug_dispatch.hpp \
+ /home/rifkireysaad/Documents/KP/WeightBride/build/OpenPLC\ Simulator/src/debug_table.hpp
---
+++ plc_program/build/OpenPLC Simulator/src/defines.h
@@ -0,0 +1,26 @@
+
+
+//Program MD5
+#define PROGRAM_MD5 "b0f44df0e86db2123b6727789b162c6f"
+
+//Comms Configuration
+#define SIMULATOR_MODE
+#define MBSERIAL_IFACE Serial
+#define MBSERIAL_BAUD 115200
+#define MBSERIAL_SLAVE 1
+#define MBSERIAL
+#define MODBUS_ENABLED
+
+
+//IO Config
+#define PINMASK_DIN 
+#define PINMASK_AIN 
+#define PINMASK_DOUT 
+#define PINMASK_AOUT 
+#define NUM_DISCRETE_INPUT 0
+#define NUM_ANALOG_INPUT 0
+#define NUM_DISCRETE_OUTPUT 0
+#define NUM_ANALOG_OUTPUT 0
+
+
+//Arduino libraries
